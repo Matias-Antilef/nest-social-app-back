cf89413b88d808c4786b5b5dfaaa63df6b4389ad
Matias-Antilef <matias.romero.dv@gmail.com>
2025-02-16 21:56:18 -0300

    cicd

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,12 @@ import { AuthController } from './auth/auth.controller';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-import { CommentModule } from './comment/comment.module';
-import { CommentController } from './comment/comment.controller';
 import { PostController } from './post/post.controller';
 import { UserController } from './user/user.controller';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [CommentModule, AuthModule, PostModule, UserModule],
-  controllers: [AppController, AuthController, UserController, PostController, CommentController],
+  imports: [AuthModule, PostModule, UserModule],
+  controllers: [AppController, AuthController, UserController, PostController],
   providers: [AppService],
 })
 export class AppModule {}
